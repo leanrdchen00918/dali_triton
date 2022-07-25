cf8e567da3a209fcb68ebbf4a5087c693fed846b
leanrdchen00918 <LEO9183212010@126.com>
2022-07-25 19:04:43 +0800

    add config file

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ cd build && cmake .. && make && ./main && cd ..
 mkdir -p model_repo/mydali/1
 cp build/libop.so model_repo/libop.so
 cp build/model.dali model_repo/mydali/1/model.dali
+cp config.pbtxt model_repo/mydali/config.pbtxt
 
-sudo docker run --gpus=1 --rm -p8000:8000 -p8001:8001 -p8002:8002 -v /home/yychen/Work/triton/test/dali_triton/model_repo/:/models \
+sudo docker run --gpus=1 --rm -p8000:8000 -p8001:8001 -p8002:8002 -v /model_repo:/models \
 nvcr.io/nvidia/tritonserver:22.06-py3 \
 tritonserver --model-repository=/models --backend-config dali,plugin_libs=/models/libop.so
